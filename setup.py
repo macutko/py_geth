ce46f18bbba95572ef24ad62d57d00dd6312756b
matus gallik <matusgallik008@gmail.com>
2021-01-05 11:07:01 +0100

    fixed release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyGeth",
-    version="0.0.3",
+    version="0.0.5",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
@@ -18,7 +18,7 @@ setuptools.setup(
     description='Pre-release of the unofficial PyGeth',
     author='macutko',
     author_email='matusgallik008@gmail.com',
-    install_requires=['web3>=5.13.1'],
+    install_requires=['web3>=5.12.0'],
     url='https://github.com/macutko/py_geth',
     download_url='https://github.com/macutko/py_geth/archive/0.0.3.tar.gz',
     keywords=['geth', 'pyGeth', 'blockchain', 'ethereum', 'py-solc', 'python solidity', 'python blockchain'],
