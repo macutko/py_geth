c63ad2bab88cf0d07c7b3a87cc34a066c2306015
matus gallik <matusgallik008@gmail.com>
2021-01-05 10:20:01 +0100

    editing setup

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
-from distutils.core import setup
-
-setup(
-    name='pyGeth',
-    packages=['pyGeth'],
-    version='0.0.1',  # Ideally should be same as your GitHub release tag varsion
-    description='Pre-release of the unnoficial PyGeth',
-    author='macutko',
-    author_email='matusgallik008@gmail.com',
-    url='https://github.com/macutko/py_geth',
-    download_url='https://github.com/macutko/py_geth/archive/0.0.1.tar.gz',
-    keywords=['geth', 'pyGeth', 'blockchain', 'ethereum', 'py-solc', 'python solidity', 'python blockchain'],
-    classifiers=[],
-)
-
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
