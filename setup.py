d6e9d2a0a641b55e050d9fcaa943699f3c171597
matus gallik <matusgallik008@gmail.com>
2021-01-05 11:08:32 +0100

    save

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyGeth",
-    version="0.0.5",
+    version="1.0.0",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
@@ -20,6 +20,6 @@ setuptools.setup(
     author_email='matusgallik008@gmail.com',
     install_requires=['web3>=5.12.0'],
     url='https://github.com/macutko/py_geth',
-    download_url='https://github.com/macutko/py_geth/archive/0.0.3.tar.gz',
+    download_url='https://github.com/macutko/py_geth/archive/1.0.0.tar.gz',
     keywords=['geth', 'pyGeth', 'blockchain', 'ethereum', 'py-solc', 'python solidity', 'python blockchain'],
 )
