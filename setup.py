5c2a3aedc8138ff290fbcb4310c842bd662298bc
matus gallik <matusgallik008@gmail.com>
2021-01-09 15:34:06 +0100

    Bump version: 3.0.0 → 4.0.0

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setuptools.setup(
     author_email='matusgallik008@gmail.com',
     install_requires=['web3>=5.12.0'],
     url='https://github.com/macutko/py_geth',
-    download_url='https://github.com/macutko/py_geth/archive/3.0.0.tar.gz',
+    download_url='https://github.com/macutko/py_geth/archive/4.0.0.tar.gz',
     keywords=['geth', 'pyGeth', 'blockchain', 'ethereum', 'py-solc', 'python solidity', 'python blockchain'],
 )
