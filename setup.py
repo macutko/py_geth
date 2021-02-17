48b885b90e36ffbc0cfcbd62c9f517be6a4f439e
Matus Gallik <matusgallik008@gmail.com>
2021-02-17 05:42:50 +0100

    feat: adding Linux support

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class PostInstallCommand(install):
 
 setuptools.setup(
     name="python_geth",
-    version="1.7.26",
+    version="1.7.28",
     long_description=long_description,
     long_description_content_type="text/markdown",
     test_suite='python_geth.tests.get_suite',
