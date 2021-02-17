e2fe12a79d03ec0fbcab7f7a4e9164b970fa28da
Matus Gallik <matusgallik008@gmail.com>
2021-02-17 12:39:56 +0100

    Bump version: 1.7.40 → 1.7.41

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class PostInstallCommand(install):
 
 setuptools.setup(
     name="python_geth",
-    version="1.7.40",
+    version="1.7.41",
     long_description=long_description,
     long_description_content_type="text/markdown",
     test_suite='python_geth.tests.get_suite',
